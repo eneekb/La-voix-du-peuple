c9cea338289060560d1d126e848fe7abd05d63d6
eneekb <contact@sebastienbruand.fr>
2024-08-13 22:20:59 +0200

    bonne première session

---
+++ main.js
@@ -0,0 +1,8 @@
+
+import open from 'open';
+
+// Remplacez l'URL par le chemin de votre image
+const imageUrl = 'C:\Users\conta\OneDrive - ETS Bruand Sebastien\Sebastien\Activité - 5 - Divers\codage javascript\tests libres\La-voix-du-peuple\carte1.png';
+
+// Ouvre l'image dans le navigateur par défaut
+open(imageUrl);
