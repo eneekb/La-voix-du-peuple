a50990a0a596adf47ddf000695f79c128e415fbc
eneekb <contact@sebastienbruand.fr>
2024-08-14 18:15:10 +0200

    2ème session: ajout de la modification d'1 pixel sur la carte

--- main.mjs
+++ main.mjs
@@ -1,8 +1,22 @@
+//import des fonctions
+import { ouvrirImageDansNavigateur } from './src/afficherimage.mjs';
+import { obtenirPixels, modifierPixels } from './src/modifpxl.mjs';
 
-import open from 'open';
+ouvrirImageDansNavigateur('carte1.png')
 
-// Remplacez l'URL par le chemin de votre image
-const imageUrl = 'C:\Users\conta\OneDrive - ETS Bruand Sebastien\Sebastien\Activité - 5 - Divers\codage javascript\tests libres\La-voix-du-peuple\carte1.png';
+const pixels = await obtenirPixels('carte1.png');
+ 
+// Afficher les premiers pixels pour vérifier le résultat
+console.log('Les premiers pixels de l\'image:', pixels.slice(0, 10));
+ 
 
-// Ouvre l'image dans le navigateur par défaut
-open(imageUrl);
+
+
+await modifierPixels('carte1.png', [{ x: 5, y: 0, couleur: { r: 255, g: 0, b: 0, a: 255 } }]);
+
+// Réouvrir l'image pour voir les changements
+ouvrirImageDansNavigateur('carte1.png');
+
+// Afficher les premiers pixels de l'image modifiée
+const pixelsModifies = await obtenirPixels('carte1.png');
+console.log('Les premiers pixels de l\'image modifiée:', pixelsModifies.slice(0, 10));
