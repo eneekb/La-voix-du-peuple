db18f4f1ae5bde081b2f082b479290a8dad0e3ac
eneekb <contact@sebastienbruand.fr>
2024-08-18 13:20:13 +0200

    ajout d'un pixel rouge sur la carte du monde quand on clique sur le bouton

---
+++ js/fonctions.js
@@ -0,0 +1,47 @@
+const canvas = document.getElementById('cartedumonde');
+const ctx = canvas.getContext('2d');
+
+// Charger la carte initiale
+export function ChargerLaCarteInitiale() {
+    const img = new Image();
+    img.src = "images/carte0.png";
+    img.onload = function() {
+        ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+    };
+}
+
+// Modifier certains pixels de l'image
+export function ModifierPixels() {
+    
+    // Obtenir les données de l'image
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    const data = imageData.data;
+    console.log(data)
+
+    // Tire un nbre entier aléatoire et le mets dans la variable nbrealeatoire
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    let nbrealeatoire = getRandomInt(0, data.length/4);
+    console.log(nbrealeatoire)
+    
+    // Colorer un pixel aléatoire en rouge
+    data[nbrealeatoire*4] = 255
+    data[nbrealeatoire*4+1] = 0
+    data[nbrealeatoire*4+2] = 0
+
+
+    // for (let i = 0; i < data.length; i += 4) {
+    //     // Change les pixels au centre du canvas en rouge
+    //     if (data[i] > 200 && data[i + 1] > 200 && data[i + 2] > 200) {
+    //         data[i] = 255;     // Rouge
+    //         data[i + 1] = 0;   // Vert
+    //         data[i + 2] = 0;   // Bleu
+    //     }
+    // }
+
+    
+    // Mettre à jour le canvas avec les nouvelles données de l'image
+    ctx.putImageData(imageData, 0, 0);
+
+}   
--- js/script.js
+++ js/script.js
@@ -1,48 +1,17 @@
 // Récupération des éléments HTML
-const canvas = document.getElementById('cartedumonde');
-const ctx = canvas.getContext('2d');
 const modifyButton = document.getElementById('modifyButton');
-const saveButton = document.getElementById('saveButton');
 
-// Initialisation de l'image blanche
-function drawWhiteImage() {
-    // Remplir le canvas avec une couleur blanche
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-}
 
-// Modifier certains pixels de l'image
-function modifyPixels() {
-    // Obtenir les données de l'image
-    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    const data = imageData.data;
+// Import des fonctions nécessaires
+import {ChargerLaCarteInitiale} from './fonctions.js';
+import {ModifierPixels} from './fonctions.js';
 
-    // Exemple : Modifier les pixels en rouge à un certain endroit
-    for (let i = 0; i < data.length; i += 4) {
-        // Change les pixels au centre du canvas en rouge
-        if (data[i] > 200 && data[i + 1] > 200 && data[i + 2] > 200) {
-            data[i] = 255;     // Rouge
-            data[i + 1] = 0;   // Vert
-            data[i + 2] = 0;   // Bleu
-        }
-    }
-    
-    // Mettre à jour le canvas avec les nouvelles données de l'image
-    ctx.putImageData(imageData, 0, 0);
-}
 
-// Sauvegarder l'image modifiée
-function saveImage() {
-    // Créer un lien pour télécharger l'image
-    const link = document.createElement('a');
-    link.href = canvas.toDataURL('image/png');
-    link.download = 'modified-image.png';
-    link.click();
-}
+
+ChargerLaCarteInitiale();
+
+
 
 // Événements des boutons
-modifyButton.addEventListener('click', modifyPixels);
-saveButton.addEventListener('click', saveImage);
+modifyButton.addEventListener('click', ModifierPixels);
 
-// Dessiner l'image blanche initiale
-drawWhiteImage();
